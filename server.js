3070564940e3d23b65391a7bc6b32ee742e5a649
Casandra Mcdonald <casandramcdonald@MacBook-Pro.local>
2021-04-10 10:48:39 -0400

    express and port up and running

--- server.js
+++ server.js
@@ -11,3 +11,13 @@ const app = express();
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Basic route that sends the user first to the AJAX Page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'public/tables.html')));
+
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'public/reserve.html')));
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
