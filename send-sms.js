f6c5e788bff304d7eeea3428f169d34a43dfa267
Casandra Mcdonald <casandramcdonald@MacBook-Pro.local>
2021-04-10 14:14:50 -0400

    added twilio

--- send-sms.js
+++ send-sms.js
@@ -1,12 +1,12 @@
-  const accountSid = 'AC0e81cd580dd50200cdb67be40bb9623d'
-  const authToken = '1cd8e30d6e5f594f379dfadfa94e5d06'
+  const accountSid = ''
+  const authToken = ''
   const client = require('twilio')(accountSid, authToken)
 
   const textMessage = ({customerName, phoneNumber}) =>{
   client.messages
     .create({
       body: `Hi ${customerName}, your table is ready!`,
-      from: '+14156920294', // example format: +15017122661
+      from: '', // example format: +15017122661
       to: `+1${phoneNumber}` // example format: +15558675310
     })
     .then(message => console.log("Message Sent!"))
